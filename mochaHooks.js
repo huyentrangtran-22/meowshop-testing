bb31b20e62062161226c4292a0e0cffb99c1b469
Trang <huyentrangt0504@gmail.com>
2026-05-24 02:22:05 +0700

    Update mochaHooks.js

--- mochaHooks.js
+++ mochaHooks.js
@@ -1,39 +1,41 @@
-const createLogger = require("./utils/logger");
-const { createBug } = require("./utils/jira");
+module.exports = function () {
+    const createLogger = require("./utils/logger");
+    const { createBug } = require("./utils/jira");
 
-let logger;
-let testName;
+    let logger;
+    let testName;
 
-beforeEach(function () {
-    testName = this.currentTest.title;
+    beforeEach(function () {
+        testName = this.currentTest.title;
 
-    logger = createLogger(testName);
-    logger.log("===== START TEST =====");
-    logger.log("Test: " + testName);
-});
+        logger = createLogger(testName);
+        logger.log("===== START TEST =====");
+        logger.log("Test: " + testName);
+    });
 
-afterEach(async function () {
-    const state = this.currentTest.state;
+    afterEach(async function () {
+        const state = this.currentTest.state;
 
-    if (state === "passed") {
-        logger.log("STATUS: PASSED");
-    }
-
-    if (state === "failed") {
-        const error = this.currentTest.err?.message || "Unknown error";
-
-        logger.log("STATUS: FAILED");
-        logger.log("ERROR: " + error);
-
-        try {
-            await createBug({
-                title: `${testName} - ${error}`,
-                logFile: logger.logFile
-            });
-        } catch (err) {
-            console.error("Create Jira bug failed:", err.message);
+        if (state === "passed") {
+            logger.log("STATUS: PASSED");
         }
-    }
 
-    logger.log("===== END TEST =====\n");
-});
+        if (state === "failed") {
+            const error = this.currentTest.err?.message || "Unknown error";
+
+            logger.log("STATUS: FAILED");
+            logger.log("ERROR: " + error);
+
+            try {
+                await createBug({
+                    title: `${testName} - ${error}`,
+                    logFile: logger.logFile
+                });
+            } catch (err) {
+                console.error("Jira error:", err.message);
+            }
+        }
+
+        logger.log("===== END TEST =====\n");
+    });
+};
